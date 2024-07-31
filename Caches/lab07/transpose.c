075d52bc16fe270a1cbc3a54d925eb97edee937b
Noman Rafiq <noman.rafiq@10xengineers.ai>
2024-07-31 11:37:14 +0500

    Caches - Task 4 (completed)

--- Caches/lab07/transpose.c
+++ Caches/lab07/transpose.c
@@ -13,4 +13,15 @@ void transpose_naive(int n, int blocksize, int *dst, int *src) {
  * multiple of the block size. */
 void transpose_blocking(int n, int blocksize, int *dst, int *src) {
     // YOUR CODE HERE
+    int i, j, ii, jj;
+    for (i = 0; i < n; i += blocksize){
+    	for (j = 0; j < n; j += blocksize){
+    		for (ii = i; ii < i + blocksize && ii < n; ++ii){
+    			for (jj = j; jj < j + blocksize && jj < n; jj++){
+    				dst [jj + ii*n] = src [ii + jj*n];
+    				}
+    			}
+    		}
+    }
+    
 }
